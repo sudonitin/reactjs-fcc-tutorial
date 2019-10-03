c9b807b6f9f2c9c5f49ba65a6298a8400fc755ca
globefire <sahun5774@gmail.com>
2019-10-03 17:12:30 +0530

    functional components
    in this commit i learned how to use functional components in indexjs file and how to import and use css on the page

--- fcc/src/index.js
+++ fcc/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-ReactDOM.render(
-<ul>
-    <li>dipu</li>
-    <li>sahu</li>
-    <li>nitin</li>
-</ul>, 
-    
-    document.getElementById('root'));
+import './index.css';
 
+function MyInfo(){
+    // return 
+    return  <div>
+                <h1>hello nitin</h1>
+                <p>My name is nitin Sahu, I love coding. I am 21 years old</p>
+                <ul>
+                    <li>Goa</li>
+                    <li>Puri</li>
+                    <li>Pune</li>
+                </ul>
+            </div>
+}
+
+ReactDOM.render(<MyInfo />,document.getElementById('root'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
