c7930f79a4077df27fba8bc9102cdda66ac2d760
globefire <sahun5774@gmail.com>
2019-11-29 19:13:57 +0530

    state practice 2

---
+++ fcc/src/components/state-practice-1.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      name: "nitin",
+      age: 21,
+      
+    }
+  }
+  render(){
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+        <h3>{this.state.age} years old</h3>
+      </div>
+    )
+  }
+}
+
+export default App
